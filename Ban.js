c0aec86e2ed4976989d488833914a672d7adfc60
nocario <worldofdovacario@gmail.com>
2022-04-26 14:46:25 +0200

    code made fully function

--- Ban.js
+++ Ban.js
@@ -1,5 +1,5 @@
 const R = require('ramda')
-const {find, assoc, cond} = require("ramda");
+
 
 
 
@@ -7,36 +7,38 @@ const BannedGames = [
     "fortnite",
     "fallout"
 ]
-//const INTERVALL_CHECK_WARNING = 30 * 60 * 1000;
-//const INTERVALL_CHECK_BAN = 5 * 60 * 1000;
-const INTERVALL_CHECK_WARNING = 5 * 1000;
-const INTERVALL_CHECK_BAN = 2 * 1000;
+//const INTERVAL_CHECK_WARNING = 30 * 60 * 1000;
+//const INTERVAL_CHECK_BAN = 5 * 60 * 1000;
+const INTERVAL_CHECK_WARNING = 5 * 1000;
+const INTERVAL_CHECK_BAN = 2 * 1000;
 
-const checkPlayedGameAtInterval = (client) => setInterval(() => checkOnlineUsersPlayedGame(client), INTERVALL_CHECK_WARNING);
+const checkPlayedGameAtInterval = (client) => setInterval(() => checkOnlineUsersPlayedGame(client), INTERVAL_CHECK_WARNING);
 
 //----------------------------------------------------------------------------------------------------------------------
 const constructObjectMemberGamePlayed =  (member) => {
     return {'user': member, 'gamePlayed': member.presence?.activities
-            .find(activity => activity.type === 'PLAYING').name.toLowerCase()}
+            .find(activity => activity.type.toString() === 'PLAYING').name.toLowerCase()}
 }
 const sendWarning = async (member) => member.user.guild.channels.cache.find(channel => channel.name === "general")
     .send(`${member.user} you are playing a forbidden game, stop if you don't want to get ban`);
 
-const checkIfPlayingGame = (member) => member.presence?.activities.find(activity => activity.type === 'PLAYING');
+const checkIfPlayingGame = (member) => member.presence?.activities.find(activity => activity.type.toString() === 'PLAYING');
+
 const checkIfPlayingForbiddenGame = (bannedGames, member) => bannedGames.includes(member?.presence?.activities
-    .find(activity => activity.type === 'PLAYING').name.toLowerCase());
+    .find(activity => activity.type.toString() === 'PLAYING').name.toLowerCase());
 const curriedCheckIfPlayingForbiddenGame = R.curry(checkIfPlayingForbiddenGame)(BannedGames);
 
-const timeOutWarning = (warned) => setTimeout(() => checkWarnedMember(warned), 2000)
+
 
 const checkIfMemberIsOnline = async (guild) => await guild.members.fetch({withPresences: true});
 const getOnlineMembers = R.pipe(R.map(checkIfMemberIsOnline));
 const getGuilds = (client) => client.guilds.cache.map(guild => client.guilds.cache.get(guild.id));
-const concatGuildsOnlineMember = (arrays) => {
-    const newArr = [];
-    for (const arr of arrays) newArr.push(...arr.values())
-    return newArr;
-}
+
+const t = (arr) => [...arr.values()];
+const concatGuildsOnlineMember = R.pipe(R.map(t), R.flatten);
+
+const timeOutWarning = (warned) => setTimeout(() => checkWarnedMember(warned), INTERVAL_CHECK_BAN)
+
 const checkOnlineUserPlayedGame2 = R.pipe(
     getGuilds,
     getOnlineMembers,
@@ -51,78 +53,38 @@ const checkOnlineUserPlayedGame2 = R.pipe(
 const finish = R.pipe(checkOnlineUserPlayedGame2, R.andThen(timeOutWarning))
 const checkOnlineUsersPlayedGame = async (client) => await finish(client);
 
-//----------------------------------------------------------------------------------------------------------------------
-
-const findIfPlaying = R.pipe(R.find(R.propEq('type', 'PLAYING')));
-const checkIfStillPlayingBannedGame = (game, gamePlayed) => R.equals(R.toLower(R.prop('name', game)), gamePlayed);
-const test = (element) => R.cond([
-    [findIfPlaying(element.user.presence.activities),
-        checkIfStillPlayingBannedGame(findIfPlaying(element.user.presence.activities), R.prop('gamePlayed', element))]
-    ]
-)
-
-const f3 = R.pipe(R.filter(test));
-const checkWarnedMember = (warned) => banWarnedMember(R.map(R.prop('user'), f3(warned)));
-/*
-const checkWarnedMember = (warnedMember) => {
-    const banList = [];
-    console.log(f3(warnedMember));
-    warnedMember.forEach(element => {
-        const member = element.user;
-        const gamePlayed = element.gamePlayed;
-        const game = member.presence.activities.find(activity => activity.type === 'PLAYING');
-        if (game) {
-            if (game.name.toLowerCase() === gamePlayed) banList.push(member);
-        }
-    })
-    banWarnedMember(banList);
-}
-
- */
 
 //----------------------------------------------------------------------------------------------------------------------
 
-const annonceUserNotBannable = (member) => member.guild.channels.cache.find(channel => channel.name === "general")
-                                .send(`${member.user} cannot be banned unfortunately`);
+const announceUserNotBannable = (member) => member.guild.channels.cache.find(channel => channel.name === "general")
+    .send(`${member.user} cannot be banned unfortunately`);
+const announceUserBan = (member) => member.guild.channels.cache.find(channel => channel.name === "general")
+    .send(`${member.user} has been banned`);
 
-
-
-const annonceUserBan = (member) => member.guild.channels.cache.find(channel => channel.name === "general")
-        .send(`${member.user} has been banned`);
-
-const banUser = R.pipe(R.invoker(0, 'ban'), R.andThen(annonceUserBan));
+//const banUser = R.pipe(R.invoker(0, 'ban'), R.andThen(announceUserBan));
 const f = R.invoker(1, "disableCommunicationUntil");
 const muteUser = (member) => {
-    const m = Date.now() + 1 * 60 * 1000;
+    const m = Date.now() + 60 * 1000;
     f(m, member);
-    annonceUserBan(member);
+    announceUserBan(member);
 }
-const checkAndBan = R.pipe(
-    R.ifElse(
-        R.prop("bannable"),
-        muteUser,  //j'utilise mute pour pas avoir à me unban a chaque fois
-        annonceUserNotBannable
-        ));
+const banWarnedMember = R.pipe(R.forEach(R.ifElse(
+    R.prop('bannable'),
+    muteUser,
+    announceUserNotBannable
+)));
 
-const banWarnedMember = R.pipe(R.forEach(checkAndBan));
-/*
-const banWarnedMember = (banList) => {
-    banList.forEach(member => {
-            checkAndBan(member);
+//----------------------------------------------------------------------------------------------------------------------
 
-            if (member.bannable) {
-                member.ban();
-            }
-            else {
-                const general = member.guild.channels.cache.find(channel => channel.name === "general");
-                general.send(`${member.user} cannot be banned unfortunately`);
-            }
+const checkIfStillPlayingGame = R.pipe(R.prop('user'), checkIfPlayingGame);
+const checkIfPLayingSameBannedGame = R.pipe(R.prop('user'), curriedCheckIfPlayingForbiddenGame)
+const checkWarnedMember = R.pipe(
+    R.filter(checkIfStillPlayingGame),
+    R.filter(checkIfPLayingSameBannedGame),
+    R.map(R.prop('user')),
+    banWarnedMember);
+
+//----------------------------------------------------------------------------------------------------------------------
 
 
-        }
-    )
-}
-
- */
-
-module.exports = {checkPlayedGameAtInterval, BannedGames};
+module.exports = {checkPlayedGameAtInterval};
